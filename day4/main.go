7bb7d239112285c669a3269499b01e66ecff6b9f
RoryC <rory.crickmore@kablamo.com.au>
2026-01-06 11:59:21 +1100

    with goroutines

--- day4/main.go
+++ day4/main.go
@@ -3,6 +3,7 @@ package day4
 import (
 	"aoc/day4/scanner"
 	_ "embed"
+	"sync"
 )
 
 //go:embed example.txt
@@ -15,7 +16,33 @@ func Main(puzzle int) int {
 	if puzzle == 1 {
 		inputData := scanner.FileToInput(input)
 		scans := scanner.NewScanner(inputData)
+
 		return scans.ScanForXmas(0, len(inputData)-1)
 	}
 	return 0
+}
+
+// Quick and dirty hardcode to see how much faster with goroutines
+func MainWithGoroutines(puzzle int) int {
+	total := 0
+	inputData := scanner.FileToInput(input)
+	scans := scanner.NewScanner(inputData)
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go func() {
+		defer wg.Done()
+		total += scans.ScanForXmas(0, 49)
+	}()
+	go func() {
+		defer wg.Done()
+		total += scans.ScanForXmas(50, 100)
+	}()
+	go func() {
+		defer wg.Done()
+		total += scans.ScanForXmas(101, 139)
+	}()
+
+	wg.Wait()
+	return total
 }
--- main.go
+++ main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"aoc/day4"
 	"fmt"
+	"time"
 )
 
 func main() {
 
+	start := time.Now()
 	output := day4.Main(1)
+	duration := time.Since(start)
 	fmt.Println("Part1")
 	fmt.Println(output)
+	fmt.Printf("time taken in us: %d\n", duration.Microseconds())
+
+	start = time.Now()
+	output = day4.MainWithGoroutines(1)
+	duration = time.Since(start)
+	fmt.Println("Part1 with goroutines")
+	fmt.Println(output)
+	fmt.Printf("time taken in us: %d\n", duration.Microseconds())
 
 	// output = day4.Main(2)
 	// fmt.Println("Part2")
